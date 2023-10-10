258173ba2a545cfecb986f3260a6dacf8712ac46
ManarElsaid <elsaidmanar@gmail.com>
2023-10-10 15:22:55 -0700

    starting base model

---
+++ models/base_model.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A module defines Base class"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """defines all common attributes/methods for other classes"""
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """returns the object data"""
+        return f"[{class name}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """ updates the public instance attribute
+        updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """ returns a dictionary containing all
+        keys/values of __dict__ of the instance"""
