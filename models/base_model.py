7bb3e3898eea39df0ca624a18ceee714a19e7c00
Sarahgad <sarahgad91@gmail.com>
2023-10-11 15:46:00 +0300

    Task (3, 4, 5) Run

--- models/base_model.py
+++ models/base_model.py
@@ -1,39 +1,51 @@
 #!/usr/bin/python3
 """A module defines Base class"""
-import uuid
+from uuid import uuid4
 from datetime import datetime
-from models import storage #import storage variable
+
 
 
 class BaseModel:
     """this defines all common attributes/methods for other classes"""
+    
     def __init__(self, *args, **kwargs):
         """initialize the object"""
-        if args:
-            pass
+   
+        self.my_number = 0
+        self.name = ""
 
-        elif kwargs:
+        if kwargs:
             for k, v in kwargs.items():
-                if (k == "created_at" || k == "updated_at"):
-                    setattr(self, k, datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                if 'name' in kwargs:
+                    self.name = kwargs['name']
+                if 'my_number' in kwargs:
+                    self.my_number = kwargs['my_number']
+
+                if (k == "created_at" or k == "updated_at"):
+                    setattr(self, k, datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
                 elif (k != "__class__"):
                     setattr(self, k, v)
         
         else:
-             storage.new(self)
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            from models import storage 
+            storage.new(self)
 
     def save(self):
         """ updates the public instance attribute
         updated_at with the current datetime"""
         self.updated_at = datetime.now()
-        storage.save(self) #call save method
+        from models import storage 
+        storage.save() #call save method
 
     def to_dict(self):
         """ returns a dictionary containing all
         keys/values of __dict__ of the instance"""
         instance_dict = {'__class__': self.__class__.__name__}
         for k, v in self.__dict__.items():
-            if (k == 'created_at' || k == 'updated_at'):
+            if (k == 'created_at' or k == 'updated_at'):
                 instance_dict[k] = v.isoformat()
             else:
                 instance_dict[k] = v
@@ -42,4 +54,4 @@ class BaseModel:
 
     def __str__(self):
         """returns the object data"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.to_dict}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
