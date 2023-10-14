20c3c1965d7427587372b268b09ac2828a00772a
Sarahgad <sarahgad91@gmail.com>
2023-10-14 13:28:42 +0300

    Check user

--- models/user.py
+++ models/user.py
@@ -8,5 +8,5 @@ class User(BaseModel):
     first_name = ""
     last_name = "" 
      
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
