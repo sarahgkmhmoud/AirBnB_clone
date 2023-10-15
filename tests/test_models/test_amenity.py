a7757bf6cfe4fa687307398ba05d5b123acf3423
Sarahgad <sarahgad91@gmail.com>
2023-10-15 18:43:46 +0300

    Pycode style test models

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,66 +1,59 @@
 #!/usr/bin/python3
-"""test suit to test amenity module"""
+"""Test suite to test the Amenity class"""
+
 import unittest
-from models.base_model import BaseModel
 from models.amenity import Amenity
-from uuid import uuid4
 from datetime import datetime
-from models import storage
 import os
 
 
-#don't forget add pycode style test
-#don't forget add Doc style test
-#don't forget documented the test models
+class TestAmenityConstructorAverageCase(unittest.TestCase):
+    """This class tests the methods under the Amenity class"""
 
-
-class test_Amenity_Constructor_averageCase(unittest.TestCase):
-    """this class to test the methods under the Amenity class"""
     @classmethod
-    def setUp(self):
-        self.A1 = Amenity()
-        dictionary = self.A1.to_dict()
-        self.A2 = Amenity(** dictionary)
-        self.dictionary2 = self.A2.to_dict()
-        self.A3 = Amenity()
-        self.A3.name = 'WiFi'
-        self.A3.id = 20
-        self.dictionary3 = {
-                             '__class__': Amenity,
-                             'name': 'breakfast'
-                             }
-        self.A4 = Amenity(**self.dictionary3)
-        self.dictionary4 = self.A4.to_dict()
-        self.A5 = Amenity(**self.dictionary3)
+    def setUp(cls):
+        cls.A1 = Amenity()
+        dictionary = cls.A1.to_dict()
+        cls.A2 = Amenity(**dictionary)
+        cls.A3 = Amenity()
+        cls.A3.name = 'WiFi'
+        cls.A3.id = 20
+        cls.dictionary3 = {
+            '__class__': Amenity,
+            'name': 'breakfast'
+        }
+        cls.A4 = Amenity(**cls.dictionary3)
+        cls.A5 = Amenity(**cls.dictionary3)
 
-
-    def test_initialize_AmenityRegular(self):
+    def test_initialize_amenity_regular(self):
         self.assertIs(type(self.A1.id), str)
         self.assertIs(type(self.A1.created_at), datetime)
         self.assertIs(type(self.A1.updated_at), datetime)
 
-    def test_equality_created_new_Amenityinstances(self):
+    def test_equality_created_new_amenity_instances(self):
         self.assertIsNot(self.A1, self.A2)
         self.assertEqual(self.A2.updated_at, self.A1.updated_at)
         self.assertEqual(self.A2.created_at, self.A1.created_at)
         self.assertEqual(self.A2.id, self.A1.id)
 
-    def test_equality_created_from__Amenitydictionary(self):
+    def test_equality_created_from_amenity_dictionary(self):
         self.assertEqual(self.A4.name, self.A5.name)
 
     def test_equality_created_directly(self):
         self.assertEqual(self.A3.name, 'WiFi')
         self.assertEqual(self.A3.id, 20)
 
-class test_AmenityKwargsValidation(unittest.TestCase):
-    @classmethod
-    def setUp(self):
-       self.A1 = Amenity()
-       self.dictionary3 = {
-        '__class__': Amenity, 'name': 'this dictionary', 'number': 98}
-       self.A2 = Amenity(** self.dictionary3)
 
-    def test_kwargsNotExist(self):
+class TestAmenityKwargsValidation(unittest.TestCase):
+    @classmethod
+    def setUp(cls):
+        cls.A1 = Amenity()
+        cls.dictionary3 = {
+            '__class__': Amenity, 'name': 'this dictionary', 'number': 98
+        }
+        cls.A2 = Amenity(**cls.dictionary3)
+
+    def test_kwargs_not_exist(self):
         self.assertNotIn('__class__', self.A1.__dict__)
         self.assertNotIn('name', self.A1.__dict__)
         self.assertNotIn('number', self.A1.__dict__)
@@ -68,9 +61,7 @@ class test_AmenityKwargsValidation(unittest.TestCase):
         self.assertIn('created_at', self.A1.__dict__)
         self.assertIn('updated_at', self.A1.__dict__)
 
-
-    def test_kwargsExist(self):
-
+    def test_kwargs_exist(self):
         self.assertNotIn('__class__', self.A2.__dict__)
         self.assertIn('name', self.A2.__dict__)
         self.assertIn('number', self.A2.__dict__)
@@ -78,32 +69,34 @@ class test_AmenityKwargsValidation(unittest.TestCase):
         self.assertNotIn('created_at', self.A2.__dict__)
         self.assertNotIn('updated_at', self.A2.__dict__)
 
-class test_AmenityStrMethod(unittest.TestCase):
+
+class TestAmenityStrMethod(unittest.TestCase):
     @classmethod
-    def setUp(self):
-        self.A1 = Amenity()
-        self.dictionary3 = {
-        '__class__': Amenity, 'name': 'free Wifi', 'id': '20'}
-        self.A5 = Amenity(**self.dictionary3)
+    def setUp(cls):
+        cls.A1 = Amenity()
+        cls.dictionary3 = {
+            '__class__': Amenity, 'name': 'free WiFi', 'id': '20'
+        }
+        cls.A5 = Amenity(**cls.dictionary3)
 
     def test_str_method(self):
-        expected_str = f"[{self.A1.__class__.__name__}] ({self.A1.id}) {self.A1.__dict__}"
-        self.assertEqual(str(self.A1),expected_str)
-        expected_str2 = f"[{self.A5.__class__.__name__}] (20) {self.A5.__dict__}"
-        self.assertEqual(str(self.A5),expected_str2)
+        s = f"[{self.A1.__class__.__name__}] ({self.A1.id}) {self.A1.__dict__}"
+        self.assertEqual(str(self.A1), s)
+        s2 = f"[{self.A5.__class__.__name__}] (20) {self.A5.__dict__}"
+        self.assertEqual(str(self.A5), s2)
 
-class test_AmenitySaveMethod(unittest.TestCase):
+
+class TestAmenitySaveMethod(unittest.TestCase):
     @classmethod
-
-    def setUp(self):
-        self.A3 = Amenity()
+    def setUp(cls):
+        cls.A3 = Amenity()
         try:
             os.rename("file.json", "tmp")
         except IOError:
             pass
 
     @classmethod
-    def tearDown(self):
+    def tearDown(cls):
         try:
             os.remove("file.json")
         except IOError:
@@ -113,22 +106,24 @@ class test_AmenitySaveMethod(unittest.TestCase):
         except IOError:
             pass
 
-
-    def test_save_Regular(self):
+    def test_save_regular(self):
         old_updated_at = self.A3.updated_at
         self.A3.name = 'Air condition'
         self.A3.save()
         self.assertNotEqual(self.A3.created_at, self.A3.updated_at)
         self.assertNotEqual(old_updated_at, self.A3.updated_at)
 
-class test_AmenityEquality(unittest.TestCase):
+
+class TestAmenityEquality(unittest.TestCase):
     def test_equality_between_equal_instances(self):
         A6 = Amenity()
         A7 = Amenity()
         self.assertNotEqual(A6, A7)
+
     def test_inequality_between_different_instances(self):
         dictionary5 = {
-        '__class__': Amenity, 'name': 'Garden'}
+            '__class__': Amenity, 'name': 'Garden'
+        }
 
         A8 = Amenity(**dictionary5)
         A9 = Amenity(**dictionary5)
@@ -136,29 +131,31 @@ class test_AmenityEquality(unittest.TestCase):
         self.assertNotEqual(A8, A9)
 
 
-class test_AmenitySerialization(unittest.TestCase):
+class TestAmenitySerialization(unittest.TestCase):
     @classmethod
-    def setUp(self):
-        self.A10 = Amenity()
-        self.A10_dict = self.A10.to_dict()
+    def setUp(cls):
+        cls.A10 = Amenity()
+        cls.A10_dict = cls.A10.to_dict()
+
     def test_serialization_to_dict(self):
         self.assertIsInstance(self.A10_dict, dict)
-    def test_formatDateTime(self):
+
+    def test_format_date_time(self):
         self.assertIs(type(self.A10_dict['created_at']), str)
         self.assertIs(type(self.A10_dict['updated_at']), str)
 
 
-class test_AmenityDeserialization(unittest.TestCase):
+class TestAmenityDeserialization(unittest.TestCase):
     @classmethod
-    def setUp(self):
-        self.A1 = Amenity()
-        dictionary = self.A1.to_dict()
-        self.A2 = Amenity(** dictionary)
+    def setUp(cls):
+        cls.A1 = Amenity()
+        dictionary = cls.A1.to_dict()
+        cls.A2 = Amenity(**dictionary)
 
-    def test_desrialization_to_dic(self):
+    def test_deserialization_to_dict(self):
         self.assertIsNot(self.A1, self.A2)
 
-    def test_check_type_desrialization(self):
+    def test_check_type_deserialization(self):
         self.assertIs(type(self.A1.id), str)
         self.assertIs(type(self.A1.created_at), datetime)
         self.assertIs(type(self.A1.updated_at), datetime)
@@ -173,6 +170,5 @@ class test_AmenityDeserialization(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    """calling the unit test"""
+    """Calling the unit test"""
     unittest.main()
-
