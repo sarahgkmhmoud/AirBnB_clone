4345a2f65b0648ae52f50963e82f85d16ff59d71
Sarahgad <sarahgad91@gmail.com>
2023-10-16 02:21:25 +0300

    Try to dic:w

--- models/base_model.py
+++ models/base_model.py
@@ -13,6 +13,10 @@ class BaseModel:
         self.updated_at = datetime.now()
         if kwargs:
             for k, v in kwargs.items():
+                if 'name' in kwargs:
+                    self.name = kwargs['name']
+                if 'my_number' in kwargs:
+                    self.my_number = kwargs['my_number']
                 if (k == "created_at" or k == "updated_at"):
                     setattr(self, k,
                             datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
@@ -29,18 +33,28 @@ class BaseModel:
         from models import storage
         storage.save()
 
+    # def to_dict(self):
+    #     """ returns a dictionary containing all
+    #     keys/values of __dict__ of the instance"""
+    #     instance_dict = {'__class__': self.__class__.__name__}
+    #     for k, v in self.__dict__.items():
+    #         if (k == 'created_at' or k == 'updated_at'):
+    #             instance_dict[k] = v.isoformat()
+    #         else:
+    #             instance_dict[k] = v
+
+    #     return instance_dict
     def to_dict(self):
-        """ returns a dictionary containing all
-        keys/values of __dict__ of the instance"""
-        instance_dict = {'__class__': self.__class__.__name__}
-        for k, v in self.__dict__.items():
-            if (k == 'created_at' or k == 'updated_at'):
-                instance_dict[k] = v.isoformat()
-            else:
-                instance_dict[k] = v
+        """Return the dictionary of the BaseModel instance.
 
-        return instance_dict
-
+        Includes the key/value pair __class__ representing
+        the class name of the object.
+        """
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
     def __str__(self):
         """returns the object data"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
