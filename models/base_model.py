bae5d095ed354cd815a51a46712492fc35ccb83d
Sarahgad <sarahgad91@gmail.com>
2023-10-16 02:23:17 +0300

    Today

--- models/base_model.py
+++ models/base_model.py
@@ -9,8 +9,8 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """initialize the object"""
         self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
         if kwargs:
             for k, v in kwargs.items():
                 if 'name' in kwargs:
@@ -29,32 +29,22 @@ class BaseModel:
     def save(self):
         """ updates the public instance attribute
         updated_at with the current datetime"""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.today()
         from models import storage
         storage.save()
 
-    # def to_dict(self):
-    #     """ returns a dictionary containing all
-    #     keys/values of __dict__ of the instance"""
-    #     instance_dict = {'__class__': self.__class__.__name__}
-    #     for k, v in self.__dict__.items():
-    #         if (k == 'created_at' or k == 'updated_at'):
-    #             instance_dict[k] = v.isoformat()
-    #         else:
-    #             instance_dict[k] = v
-
-    #     return instance_dict
     def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
+        """ returns a dictionary containing all
+        keys/values of __dict__ of the instance"""
+        instance_dict = {'__class__': self.__class__.__name__}
+        for k, v in self.__dict__.items():
+            if (k == 'created_at' or k == 'updated_at'):
+                instance_dict[k] = v.isoformat()
+            else:
+                instance_dict[k] = v
 
-        Includes the key/value pair __class__ representing
-        the class name of the object.
-        """
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
+        return instance_dict
+
     def __str__(self):
         """returns the object data"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
